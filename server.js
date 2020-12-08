cacdb8de146596972a89470704f4810eb586280e
WondwosenTsige <wondneh@gmail.com>
2020-12-08 15:18:19 -0800

    server running

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const app = express();
-const PORT = process.env.PORT || 3001;
-app.listen(PORT);
+const PORT = process.env.PORT || 3000;
 
 
 
