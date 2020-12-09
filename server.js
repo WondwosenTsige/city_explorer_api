5ea6a81bf70c9b1351dfd75c835d0c01ea201a3c
WondwosenTsige <wondneh@gmail.com>
2020-12-08 16:04:43 -0800

    Server edition

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
+'use strict'
 
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 3000;
+app.use(cors());
+
 
 app.get('/location', function(req, res){
-  try{
+  
     const accessLocation = require('./data/location.json');
     const accessCity = req.query.city;
     const findlocation = new Location(accessLocation, accessCity);
     res.status(200).json(findlocation);
-  }
-  catch(error){
-    console.log(error, 'not working');
-  }
+  
 })
 
 app.get('/weather', function(req, res){
@@ -40,9 +40,10 @@ function Weather(weather){
   this.time = weather.valid_date;
 }
 
+// Add error handling and start server
+
 app.listen(PORT, () => console.log(`app is listening at ${PORT}`));
 
-// Add error handling and start server
 app.use('*', (request, response) => {
-    response.status(404).send('The route you are looking for has been disconnected, We hope you have a nice day');
+    response.status(404).send('ERROR LOADING PAGE');
   });
